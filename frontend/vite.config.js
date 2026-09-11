6b4a55b9a2e92e3d7e7ec83e0bacb45adec0397d
RajneeshYadav-123 <rajneeshyadav0507@gmail.com>
2026-09-11 13:32:55 +0530

    backend url chaged in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://inspectra-s4hg.vercel.app/',
         changeOrigin: true,
       }
     }
